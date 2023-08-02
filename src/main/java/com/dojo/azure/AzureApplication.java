90eddb408b74b4e4947e5cada934cf6d8939afaa
Alejo-Casta <alejo.casta.al@gmail.com>
2023-08-02 00:44:53 -0500

    Dojo Azure

---
+++ src/main/java/com/dojo/azure/AzureApplication.java
@@ -0,0 +1,21 @@
+package com.dojo.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String sayHello() {
+		return "Azure Dojo Finished";
+	}
+
+}
